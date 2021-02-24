5c6a0f0cd5c6d056a70393e0c9bbfc733fcf16af
Sark <dsarkodie500@gmail.com>
2021-02-24 20:17:00 +0000

    committed

--- route/api/posts.js
+++ route/api/posts.js
@@ -90,6 +90,7 @@ router.put('/likes/:id', auth, async(req, res) => {
         if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
             return res.status(400).json({msg: 'Post already liked'});
         }
+        //like post
         post.likes.unshift({user: req.user.id});
         await post.save()
          res.json(post.likes);
