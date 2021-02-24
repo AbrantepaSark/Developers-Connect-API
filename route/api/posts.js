8428ef2a3ecfa43d7900035d1986c865f8ccbe42
Sark <dsarkodie500@gmail.com>
2021-02-24 20:02:35 +0000

    Committed

--- route/api/posts.js
+++ route/api/posts.js
@@ -84,8 +84,11 @@ router.delete('/:postID',auth ,async(req, res) => {
 //@desc  like post
 //@access Private
 router.put('/likes/:id', auth, async(req, res) => {
+    console.log(`there`);
     try {
-        const post = Post.findById(req.params.id);
+        console.log(`here`);
+        const post = await Post.findById(req.params.id);
+        console.log(post);
         //check if post already liked by user
         if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
             return res.status(400).json({msg: 'Post already liked'});
